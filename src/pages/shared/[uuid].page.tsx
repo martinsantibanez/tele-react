5410c6644dfcc170c7b34b3ad2a6aad908f22a80
Martin Santibanez <martin.santibanez.a@gmail.com>
2023-09-12 03:15:08 -0300

    fix share

--- src/pages/shared/[uuid].page.tsx
+++ src/pages/shared/[uuid].page.tsx
@@ -1,26 +1,25 @@
-import { Redis } from '@upstash/redis';
+import { kv } from '@vercel/kv';
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useCustomSources } from '../../hooks/useCustomSources';
 import { useDisplayConfig } from '../../hooks/useDisplayConfig';
 import { useSavedGrid } from '../../hooks/useSavedGrid';
+import { useZappingConfig } from '../../hooks/useZappingConfig';
 import { MainLayout } from '../../layout/MainLayout';
 import { ScreenType } from '../monitor/types';
-import { useZappingConfig } from '../../hooks/useZappingConfig';
-import { useCustomSources } from '../../hooks/useCustomSources';
 
 export const getServerSideProps: GetServerSideProps<{
   initialScreen: ScreenType;
 }> = async context => {
   console.log(JSON.stringify(context.params));
-  const redis = Redis.fromEnv();
   const uuid = context.params?.uuid;
   if (typeof uuid !== 'string')
     return {
       notFound: true
     };
-  const initialScreen = await redis.get<ScreenType>(uuid);
+  const initialScreen = await kv.get<ScreenType>(uuid);
   if (!initialScreen)
     return {
       notFound: true
