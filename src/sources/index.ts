49145571f72f969a94dd089295f9506e8d66d5a8
Martin Santibanez <martin.santibanez@kombo.co>
2025-01-02 21:47:45 -0300

    fix: remove panamericanos

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -6,7 +6,6 @@ import { espacioSources } from './espacio';
 import { especialesSources } from './especiales';
 import { internacionalSources } from './internacional';
 import { musicaSources } from './musica';
-import { panamericanosSources } from './panamericanos';
 import { placeHolderSources } from './placeholder';
 import { radiosSources } from './radios';
 import { regionesSources } from './regiones';
@@ -56,10 +55,6 @@ export interface SourceGroup {
 }
 
 export const sourcesCategories: SourceGroup[] = [
-  {
-    name: 'Panamericanos',
-    sources: panamericanosSources
-  },
   {
     name: 'TV Nacional',
     sources: tvNacionalSources
