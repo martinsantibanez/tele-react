848c02b79d8ae3eb6b7baa6532293abbc6a750d3
Martin Santibanez <martin.santibanez@kombo.co>
2025-01-02 21:47:50 -0300

    fix: change initial grid

--- src/components/GridDisplay/initialGrid.ts
+++ src/components/GridDisplay/initialGrid.ts
@@ -2,28 +2,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { SourceNode } from '../../pages/monitor/types';
 
 export const defaultGrid: SourceNode[] = [
-  {
-    sourceSlug: '24HTVN',
-    uuid: uuidv4()
-  },
-  {
-    sourceSlug: 'T13_YT',
-    uuid: uuidv4()
-  },
-  {
-    sourceSlug: 'MEGA',
-    uuid: uuidv4()
-  },
+  { sourceSlug: '24HTVN_YT', uuid: uuidv4() },
+  { sourceSlug: 'T13_YT', uuid: uuidv4() },
+  { sourceSlug: 'MEGA', uuid: uuidv4() },
   {
     sourceSlug: 'PLAZAITALIA_CIMA',
     uuid: uuidv4()
   },
-  {
-    sourceSlug: 'cnn-us',
-    uuid: uuidv4()
-  },
-  {
-    sourceSlug: 'RELOJ_CHILE',
-    uuid: uuidv4()
-  }
+  { sourceSlug: 'TVMAS', uuid: uuidv4() },
+  { sourceSlug: 'RELOJ_CHILE', uuid: uuidv4() }
 ];
