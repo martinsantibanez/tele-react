60954227de93324d0c849f9a18da59945b70c1db
Martin Santibanez <martin.santibanez.a@gmail.com>
2026-07-27 14:07:18 -0400

    fix: initial grid

--- src/components/GridDisplay/initialGrid.ts
+++ src/components/GridDisplay/initialGrid.ts
@@ -1,14 +1,38 @@
-import { v4 as uuidv4 } from 'uuid';
 import { SourceNode } from '../../types/Monitor';
 
 export const defaultGrid: SourceNode[] = [
-  { sourceSlug: '24HTVN_YT', uuid: uuidv4() },
-  { sourceSlug: 'T13_YT', uuid: uuidv4() },
-  { sourceSlug: 'MEGA', uuid: uuidv4() },
   {
-    sourceSlug: 'PLAZAITALIA_CIMA',
-    uuid: uuidv4()
+    sourceSlug: 'custom_24-horas',
+    uuid: '72a3557a-d7f6-4af5-94b1-e59925c2be12',
+    activeSignal: 'm3u8',
+    muted: false
   },
-  { sourceSlug: 'TVMAS', uuid: uuidv4() },
-  { sourceSlug: 'RELOJ_CHILE', uuid: uuidv4() }
+  {
+    sourceSlug: 'custom_t13',
+    uuid: '2bbc3ccd-7c5c-49a0-9ffc-7ec5933f4d1f',
+    activeSignal: 'm3u8',
+    muted: true
+  },
+  {
+    sourceSlug: 'custom_chilevision',
+    uuid: 'd26245d4-4aab-429c-b674-dcc8d1a8a7c8',
+    muted: true
+  },
+  {
+    sourceSlug: 'custom_meganoticias',
+    uuid: 'ff47f8e4-be38-4b03-9f1b-cb03061b70ec',
+    activeSignal: 'm3u8',
+    muted: true
+  },
+  {
+    sourceSlug: 'custom_cnn-cl',
+    uuid: '35376c19-bdf9-4f58-9656-d2dd38eb9207',
+    activeSignal: 'm3u8',
+    muted: true
+  },
+  {
+    sourceSlug: 'RELOJ_CHILE',
+    uuid: '75749ac2-129d-4b14-9f77-492593ba1fc4',
+    muted: true
+  }
 ];
